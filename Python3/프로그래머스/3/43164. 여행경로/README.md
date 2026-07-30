21edd24e873635e51e0564b8d6952bf0c775a1f0
codml <108252974+codml@users.noreply.github.com>
2026-07-30 17:19:57 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/43164. 여행경로/README.md
+++ Python3/프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 11.2 MB, 시간: 0.04 ms
+메모리: 11.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 30일 14:47:11
+2026년 07월 30일 17:19:52
 
 ### 문제 설명
 
--- Python3/프로그래머스/3/43164. 여행경로/여행경로.py
+++ Python3/프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,35 +1,25 @@
+from collections import defaultdict
+
 def solution(tickets):
-    routes = {}
-    visited = {}
+    routes = defaultdict(list)
     
     for ticket in tickets:
-        for airport in ticket:
-            if airport not in routes:
-                routes[airport] = []
-                visited[airport] = {}
-        
         arr = ticket[0]
         dep = ticket[1]
         routes[arr].append(dep)
-        if dep in visited[arr]:
-            visited[arr][dep] += 1
-        else:
-            visited[arr][dep] = 1
         routes[arr].sort()
-
+    
     def dfs(cur, answer):
         if len(answer) - 1 == len(tickets):
             return answer
 
-        for next_ in routes[cur]:
-            if visited[cur][next_] > 0:
-                visited[cur][next_] -= 1
-                result = dfs(next_, answer + [next_])
-                visited[cur][next_] += 1
-                if result:
-                    return result
+        for idx, next_ in enumerate(routes[cur]):
+            routes[cur].pop(idx)
+            result = dfs(next_, answer + [next_])
+            routes[cur].insert(idx, next_)
+            if result:
+                return result
         return None
-
-    answer = dfs('ICN', ['ICN'])
     
+    answer = dfs('ICN', ['ICN'])
     return answer
