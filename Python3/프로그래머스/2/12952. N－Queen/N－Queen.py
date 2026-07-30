858378d9cdb5390b75714be73a430ede169e9ad0
codml <108252974+codml@users.noreply.github.com>
2026-07-30 18:55:52 +0900

    [level 2] Title: N-Queen, Time: 3653.77 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/12952. N－Queen/N－Queen.py
+++ Python3/프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,23 +1,24 @@
-answer = 0
+
 
 def solution(n):
-    
     chess = [0 for _ in range(n)]
     
-    def is_possible(cur, col):
+    def is_possible(cur, col, chess):
         for i in range(cur):
             if chess[i] == col or chess[i] - col == i - cur or chess[i] - col == cur - i:
                 return False
         return True
     
-    def backtrack(cur):
+    def backtrack(cur, chess):
         if cur == n:
-            global answer
-            answer += 1
+            return 1
+        
+        answer = 0
         
         for i in range(n):
-            if is_possible(cur, i):
+            if is_possible(cur, i, chess):
                 chess[cur] = i
-                backtrack(cur + 1)
-    backtrack(0)
-    return answer
+                answer += backtrack(cur + 1, chess)
+                
+        return answer
+    return backtrack(0, chess)
